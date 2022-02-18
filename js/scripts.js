8d15f304874042651248a93b28b1d07036fcaf55
Matthew <78713317+mreed4@users.noreply.github.com>
2022-02-18 08:56:25 -0800

    JS now works; CSS: make page look like the product

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,42 @@
-let body = document.body;
-let container = document.querySelector("#container");
-let gridItem;
 let gridSize = 16;
 
-for (let i = 1; i <= gridSize ** 2; i++) {
-  gridItem = document.createElement("div");
-  container.appendChild(gridItem);
-  gridItem.innerHTML = `<span class='grid-item'>${i}</span>`;
-}
+let screen = document.querySelector("#screen");
+screen.style.cssText = `
+  grid-template-columns: repeat(${gridSize}, 1fr); 
+  grid-template-rows: repeat(${gridSize}, 1fr);
+  width: ${gridSize ** 2 * 2}px;
+  height: ${gridSize ** 2 * 2}px;
+`;
+let gridItem;
 
-gridItem.addEventListener("mouseover", () => {
-  gridItem.style.backgroundColor = "red";
-});
+const createDivs = (gridSize) => {
+  for (let i = 1; i <= gridSize ** 2; i++) {
+    gridItem = document.createElement("div");
+    screen.appendChild(gridItem);
+    gridItem.classList.add("grid-item");
+    gridItem.innerHTML = `<span class="item-label">${i}</span>`;
+  }
+
+  changeGridItems();
+};
+
+const changeGridItems = () => {
+  let allGridItems = [...screen.children];
+  allGridItems.forEach((n) => {
+    n.addEventListener("mouseover", () => {
+      n.style.backgroundColor = "black";
+      n.style.opacity = "0.2";
+      n.style.border = "1px solid gainsboro";
+    });
+  });
+};
+
+const resetGrid = () => {
+  let allGridItems = [...screen.children];
+  allGridItems.forEach((n) => {
+    n.style.backgroundColor = "none";
+    n.style.border = "0";
+  });
+};
+
+createDivs(gridSize);
